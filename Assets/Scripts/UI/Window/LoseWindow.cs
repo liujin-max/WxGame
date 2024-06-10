ff44ef87599aabe9dae3fd6966efc8fe7444297c
liujin-max <387616939@qq.com>
2024-06-10 16:50:55 +0800

    失败动画

--- Assets/Scripts/UI/Window/LoseWindow.cs
+++ Assets/Scripts/UI/Window/LoseWindow.cs
@@ -7,11 +7,32 @@ public class LoseWindow : MonoBehaviour
 {
     [SerializeField] Button m_BtnRestart;
 
+    private CDTimer m_Timer = new CDTimer(1.5f);
+
+    void Awake()
+    {
+        m_BtnRestart.gameObject.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        // m_BtnContinue.onClick.AddListener(()=>{
+        m_BtnRestart.onClick.AddListener(()=>{
+            GameFacade.Instance.EffectManager.Load(EFFECT.SWITCH, Vector3.zero, UIManager.EFFECT.gameObject).GetComponent<SceneSwitch>().Enter(()=>{
+                Field.Instance.Dispose();
+                Field.Instance.Enter(Field.Instance.Stage.ID);
 
-        // });
+                GameFacade.Instance.UIManager.UnloadWindow(gameObject);
+            });     
+        });
+    }
+
+    void Update()
+    {
+        m_Timer.Update(Time.deltaTime);
+        if (m_Timer.IsFinished())
+        {
+            m_BtnRestart.gameObject.SetActive(true);
+        }
     }
 }
